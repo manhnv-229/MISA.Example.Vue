97ff6869ceff2a29c082c9aed76d4adb7a9e7d7a
manhnv229 <manhnv229@gmail.com>
2021-01-18 14:23:41 +0700

    add example 2

--- misa-example-vue/src/App.vue
+++ misa-example-vue/src/App.vue
@@ -1,43 +1,32 @@
 <template>
- <div id='app'>
-    <Navbar />
-    <Header />
-    <Content />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
-  <!-- <div id='nav'>
-    <router-link to='/'>Home</router-link> |
-    <router-link to='/about'>About</router-link>
-  </div> -->
 </template>
-<script>
-import Navbar from '@/components/layout/Navbar'
-import Header from '@/components/layout/Header'
-import Content from '@/components/layout/Content'
-export default {
-  name: 'App',
-  components: {
-    Navbar,
-    Header,
-    Content
-  }
-}
-</script>
-<style lang='scss'>
-@import './styles/index.scss';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #019160;
-}
-.navbar .navbar-content .nav-item.router-link-exact-active {
-    background-color: #e5e5e5;
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- misa-example-vue/src/router/index.js
+++ misa-example-vue/src/router/index.js
@@ -1,38 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Customer from '../views/dictionary/customer/CustomerList.vue'
-import Employee from '../views/dictionary/employee/EmployeeList.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
-  {path: '/',name: 'Home',component: Home},
-  {path: '/about',name: 'About',
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/dictionary/customer',
-    name: 'Customer',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: Customer
-  },
-  {
-    path: '/dictionary/employee',
-    name: 'Employee',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: Employee
   }
-
 ]
 
-const router = createRouter({
+const router = new VueRouter({
   mode: 'history',
-  history: createWebHistory(process.env.BASE_URL),
+  base: process.env.BASE_URL,
   routes
 })
 
